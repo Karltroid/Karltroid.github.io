6cfa2f82da75a7808f992fd25a0b646fa494bb66
Karl <KarltroidContact@Gmail.com>
2017-03-15 15:10:13 -0400

    Added Some Functionality - Developer Page started!

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Karltroid.Github.io
 This is Karl Palmer(me)'s personal website!
+My main goal is to make this whole website with no loading screens.
 
 -HIGHLY WIP
 
--- js/index/scripts.js
+++ js/index/scripts.js
@@ -2,15 +2,36 @@ $(document).ready(function(){
 
 	$('div.MainText').fadeIn(1000);	//page fade-in
 
-	$('span.Developer').click(function(){
+	$('span.Developer').click(function(){ // developer clicked
 		$('div.MainText').animate({
-			'marginTop' : "-=45vw"
+			'marginTop' : "-=100vw"
+		});
+		$('div.DeveloperInfo').animate({
+			'marginTop' : "-=55vw"
+		});
+		$('div.DeveloperBackButton').css({
+			'display' : "inline"
 		});
 	});
 
-	$('span.Entertainer').click(function(){
+	$('div.DeveloperBackButton').click(function(){ // developer page back button clicked
 		$('div.MainText').animate({
-			'marginTop' : "-=45vw"
+			'marginTop' : "+=100vw"
+		});
+		$('div.DeveloperInfo').animate({
+			'marginTop' : "+=55vw"
+		});
+		$('div.DeveloperBackButton').css({
+			'display' : "none"
+		});
+	});
+
+	$('span.Entertainer').click(function(){ //entertainer clicked
+		$('div.MainText').animate({
+			'marginTop' : "+=100vw"
+		});
+		$('div.EntertainerInfo').animate({
+			'marginTop' : "-=55vw"
 		});
 	});
 
