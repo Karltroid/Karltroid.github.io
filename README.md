c66b03f83242b193bb269450c2e39a8503720493
Karl <KarltroidContact@Gmail.com>
2017-03-15 13:31:35 -0400

    Complete Re-do of the whole site! - WIP

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# https://karltroid.github.io/index.html
-#    Website ^
-# To-Do List v
-# https://docs.google.com/document/d/1sE29eVJHn1LEGuhTO7Gfnz9LAU_KPhDqC7XETVQuMJs/edit?usp=sharing
+# learning-javascript
+The purpose of this "project" is to start learning Javascript.
+At the moments I know HTML & CSS.
+
+-I am using http://www.w3schools.com/js/default.asp to learn!
---
+++ js/index/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	$('div.MainText').fadeIn(1000);	//page fade-in
+
+	$('span.Developer').click(function(){
+		$('div.MainText').animate({
+			'marginTop' : "-=39vw"
+		});
+	});
+
+	$('span.Entertainer').click(function(){
+		$('div.MainText').animate({
+			'marginTop' : "-=39vw"
+		});
+	});
+
+}); 
+
+function nothingSelected()
+{
+	document.getElementById('MainPageBackground').src='images/index/nothingSelected.png';
+}
+
+function DeveloperSelected()
+{
+	document.getElementById('MainPageBackground').src='images/index/testimageDeveloper.png';
+}
+
+function EntertainerSelected()
+{
+	document.getElementById('MainPageBackground').src='images/index/testimageEntertainer.png';
+}
