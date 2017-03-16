e6f67112b6c15cbb076cf7023e679a7a7cc60b3c
Karl <KarltroidContact@Gmail.com>
2017-03-16 18:48:23 -0400

    Functional Entertainer Page & Little Additions!

--- js/index/scripts.js
+++ js/index/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 			'marginTop' : "-=100vw"
 		});
 		$('div.DeveloperInfo').animate({
-			'marginTop' : "-=55vw"
+			'marginTop' : "-=56vw"
 		});
 		$('div.DeveloperBackButton').css({
 			'display' : "inline"
@@ -19,7 +19,7 @@ $(document).ready(function(){
 			'marginTop' : "+=100vw"
 		});
 		$('div.DeveloperInfo').animate({
-			'marginTop' : "+=55vw"
+			'marginTop' : "+=56vw"
 		});
 		$('div.DeveloperBackButton').css({
 			'display' : "none"
@@ -28,26 +28,55 @@ $(document).ready(function(){
 
 	$('span.Entertainer').click(function(){ //entertainer clicked
 		$('div.MainText').animate({
+			'marginTop' : "-=100vw"
+		});
+		$('div.EntertainerInfo').animate({
+			'marginTop' : "-=56vw"
+		});
+		$('div.EntertainerBackButton').css({
+			'display' : "inline"
+		});
+	});
+	
+	$('div.EntertainerBackButton').click(function(){ // developer page back button clicked
+		$('div.MainText').animate({
 			'marginTop' : "+=100vw"
 		});
 		$('div.EntertainerInfo').animate({
-			'marginTop' : "-=55vw"
+			'marginTop' : "+=56vw"
+		});
+		$('div.EntertainerBackButton').css({
+			'display' : "none"
+		});
+	});
+
+	$('div.ProfileInfo').mouseenter(function(){ // profile picture fading out
+		$('img.ProfilePicture').css({
+			'opacity' : ".5"
+		});
+	});
+	$('div.ProfileInfo').mouseleave(function(){ // profile picture fading in
+		$('img.ProfilePicture').css({
+			'opacity' : "1"
 		});
 	});
 
 }); 
 
-function nothingSelected()
-{
-	document.getElementById('MainPageBackground').src='images/index/nothingSelected.png';
-}
 
-function DeveloperSelected()
-{
-	document.getElementById('MainPageBackground').src='images/index/testimageDeveloper.png';
-}
+//***FEATURE DELAYED UNTIL I CAN FIND A MORE CLEAN/PLESANT WAY OF DOING IT; LOOKED TO GLITCHY & OUT OF PLACE***
+//
+//function nothingSelected()
+//{
+//	document.getElementById('MainPageBackground').src='images/index/nothingSelected.png';
+//}
 
-function EntertainerSelected()
-{
-	document.getElementById('MainPageBackground').src='images/index/testimageEntertainer.png';
-}
+//function DeveloperSelected()
+//{
+//	document.getElementById('MainPageBackground').src='images/index/testimageDeveloper.png';
+//}
+
+//function EntertainerSelected()
+//{
+//	document.getElementById('MainPageBackground').src='images/index/testimageEntertainer.png';
+//}
