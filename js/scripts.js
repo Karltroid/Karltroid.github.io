263ec8ea83ae00ee124e9311be6f7946dcbffa85
Karl <KarltroidContact@Gmail.com>
2017-07-12 20:02:47 -0400

    Fixed some images not loading on some browsers

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,9 @@
 $(document).ready(function(){
 	
 	$(function(){
-       $('.content').html('<img class="loading" src="images/loading.gif" />').load('https://rawgit.com/Karltroid/karltroid.github.io/master/_Webpages/_HomePage.html');
+        $('.content')
+			.html('<img class="loading" src="https://rawgit.com/Karltroid/karltroid.github.io/master/images/loading.gif" />')
+			.load('https://rawgit.com/Karltroid/karltroid.github.io/master/_Webpages/_HomePage.html');
     });
 	
 });	
